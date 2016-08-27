4802658cc414df8c0d7ec30d5fc62661696fd908
Arnaud Tournier <ltearno@gmail.com>
2016-08-27 09:47:13 +0200

    Adding more HTML

--- src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/ApplicationComponent.java
+++ src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/ApplicationComponent.java
@@ -11,7 +11,10 @@ import jsinterop.annotations.JsType;
  */
 @Component(
 		selector = "my-app",
-		template = "<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>" )
+		template = "<h1>{{title}}</h1>"
+				+ "<h2>{{hero.name}} details!</h2>"
+				+ "<div><label>id: </label>{{hero.id}}</div>"
+				+ "<div><label>name: </label>{{hero.name}}</div>" )
 @JsType
 public class ApplicationComponent
 {
