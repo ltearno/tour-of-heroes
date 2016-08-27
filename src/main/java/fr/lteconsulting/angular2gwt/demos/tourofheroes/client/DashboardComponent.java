d2002317e0cd31f80682c47a39a064ab4aebdbc0
Arnaud Tournier <arnaud.tournier@rigpa.org>
2016-08-27 17:09:46 +0200

    Dashboard Top Heroes

--- src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/DashboardComponent.java
+++ src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/DashboardComponent.java
@@ -1,12 +1,33 @@
 package fr.lteconsulting.angular2gwt.demos.tourofheroes.client;
 
+import fr.lteconsulting.angular2gwt.client.JsArray;
+import fr.lteconsulting.angular2gwt.client.interop.ng.core.OnInit;
 import fr.lteconsulting.angular2gwt.ng.core.Component;
 import jsinterop.annotations.JsType;
 
 @Component(
 		selector = "my-dashboard",
-		template = "<h3>My Dashboard</h3>" )
+		templateUrl = "dashboard.component.html" )
 @JsType
-public class DashboardComponent
+public class DashboardComponent implements OnInit
 {
+	public JsArray<Hero> heroes = JsArray.empty();
+
+	private HeroService heroService;
+
+	public DashboardComponent( HeroService heroService )
+	{
+		this.heroService = heroService;
+	}
+
+	@Override
+	public void ngOnInit()
+	{
+		heroService.getHeroes().then( heroes -> this.heroes = heroes.slice( 1, 5 ) );
+	}
+
+	public void gotoDetail( Hero hero )
+	{
+		// not implemented yet
+	}
 }
