a341bb2ea6013d601bf9ac990599a37b216f0d59
Arnaud Tournier <ltearno@gmail.com>
2016-08-27 11:38:51 +0200

    Displaying our Heroes

--- src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/ApplicationComponent.java
+++ src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/ApplicationComponent.java
@@ -1,5 +1,6 @@
 package fr.lteconsulting.angular2gwt.demos.tourofheroes.client;
 
+import fr.lteconsulting.angular2gwt.client.JsArray;
 import fr.lteconsulting.angular2gwt.ng.core.Component;
 import jsinterop.annotations.JsType;
 
@@ -12,12 +13,32 @@ import jsinterop.annotations.JsType;
 @Component(
 		selector = "my-app",
 		template = "<h1>{{title}}</h1>"
+				+ "<h2>My Heroes</h2>"
+				+ "<ul class='heroes'>"
+				+ "<li *ngFor='let hero of heroes'>"
+				+ "<span class='badge'>{{hero.id}}</span> {{hero.name}}"
+				+ "</li>"
+				+ "</ul>"
 				+ "<h2>{{hero.name}} details!</h2>"
 				+ "<div><label>id: </label>{{hero.id}}</div>"
-				+ "<div><label>name: </label><input [(ngModel)]='hero.name' placeholder='name'></div>" )
+				+ "<div><label>name: </label><input [(ngModel)]='hero.name' placeholder='name'></div>",
+		styleUrls = "application.component.css" )
 @JsType
 public class ApplicationComponent
 {
+	private static final JsArray<Hero> HEROES = JsArray.of(
+			new Hero( 11, "Mr. Nice" ),
+			new Hero( 12, "Narco" ),
+			new Hero( 13, "Bombasto" ),
+			new Hero( 14, "Celeritas" ),
+			new Hero( 15, "Magneta" ),
+			new Hero( 16, "RubberMan" ),
+			new Hero( 17, "Dynama" ),
+			new Hero( 18, "Dr IQ" ),
+			new Hero( 19, "Magma" ),
+			new Hero( 20, "Tornado" ) );
+
 	public String title = "Tour of Heroes";
 	public Hero hero = new Hero( 1, "Windstorm" );
+	public JsArray<Hero> heroes = HEROES;
 }
