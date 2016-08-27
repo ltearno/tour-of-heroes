e50a53efc991738c41b2a84b781d814e2c8ead0c
Arnaud Tournier <ltearno@gmail.com>
2016-08-27 09:00:18 +0200

    initial commit

---
+++ src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/ApplicationComponent.java
@@ -0,0 +1,21 @@
+package fr.lteconsulting.angular2gwt.demos.tourofheroes.client;
+
+import fr.lteconsulting.angular2gwt.ng.core.Component;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+/**
+ * Main angular component for the application.
+ * 
+ * <p>
+ * It simply binds to the <my-app> tag.
+ */
+@Component(
+		selector = "my-app",
+		template = "<h1>{{title}}</h1>You can edit the title by changing the text in this box :<br/><input [(ngModel)]='title'/>" )
+@JsType
+public class ApplicationComponent
+{
+	@JsProperty
+	private String title = "Your application is working !";
+}
---
+++ src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/ApplicationModule.java
@@ -0,0 +1,15 @@
+package fr.lteconsulting.angular2gwt.demos.tourofheroes.client;
+
+import fr.lteconsulting.angular2gwt.client.interop.ng.forms.FormsModule;
+import fr.lteconsulting.angular2gwt.client.interop.ng.platformBrowser.BrowserModule;
+import fr.lteconsulting.angular2gwt.ng.core.NgModule;
+import jsinterop.annotations.JsType;
+
+@NgModule(
+		imports = { BrowserModule.class, FormsModule.class },
+		declarations = ApplicationComponent.class,
+		bootstrap = ApplicationComponent.class )
+@JsType
+public class ApplicationModule
+{
+}
