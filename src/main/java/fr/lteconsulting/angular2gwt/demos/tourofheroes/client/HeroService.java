37edfe4a072478c6f43ed874db5b1a74723661e3
Arnaud Tournier <ltearno@gmail.com>
2016-08-30 09:59:11 +0200

    Add, Edit, Delete

--- src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/HeroService.java
+++ src/main/java/fr/lteconsulting/angular2gwt/demos/tourofheroes/client/HeroService.java
@@ -2,7 +2,10 @@ package fr.lteconsulting.angular2gwt.demos.tourofheroes.client;
 
 import com.google.gwt.core.client.GWT;
 
+import fr.lteconsulting.angular2gwt.client.JSON;
 import fr.lteconsulting.angular2gwt.client.JsArray;
+import fr.lteconsulting.angular2gwt.client.JsObject;
+import fr.lteconsulting.angular2gwt.client.interop.ng.http.Headers;
 import fr.lteconsulting.angular2gwt.client.interop.ng.http.Http;
 import fr.lteconsulting.angular2gwt.client.interop.promise.Promise;
 import fr.lteconsulting.angular2gwt.ng.core.Injectable;
@@ -13,7 +16,14 @@ import jsinterop.annotations.JsType;
 public class HeroService
 {
 	private Http http;
-	private String heroesUrl = "app/heroes"; // URL to web api
+
+	private static final String heroesUrl = "app/heroes"; // URL to web api
+	private static final Headers headers = new Headers();
+
+	static
+	{
+		headers.append( "Content-Type", "application/json" );
+	}
 
 	public HeroService( Http http )
 	{
@@ -31,6 +41,46 @@ public class HeroService
 	public Promise<Hero> getHero( int id )
 	{
 		return getHeroes().then( heroes -> heroes.find( hero -> hero.id == id ) );
+	}
+
+	public Promise<?> delete( int id )
+	{
+		String url = heroesUrl + "/" + id;
+		return http.delete( url )
+				.toPromise()
+				.then( response -> null )
+				.onCatch( this::handleError );
+	}
+
+	public Promise<Hero> save( Hero hero )
+	{
+		if( hero.id > 0 )
+			return update( hero );
+
+		hero.id = 0;
+
+		return create( hero );
+	}
+
+	public Promise<Hero> create( Hero hero )
+	{
+		return http.post( heroesUrl,
+				JSON.stringify( hero ),
+				new JsObject().set( "headers", headers ) )
+				.toPromise()
+				.<Hero> then( response -> response.json() )
+				.onCatch( this::handleError );
+	}
+
+	public Promise<Hero> update( Hero hero )
+	{
+		String url = heroesUrl + "/" + hero.id;
+		return http.put( url,
+				JSON.stringify( hero ),
+				new JsObject().set( "headers", headers ) )
+				.toPromise()
+				.<Hero> then( response -> response.json() )
+				.onCatch( this::handleError );
 	}
 
 	private Promise<?> handleError( Object error )
